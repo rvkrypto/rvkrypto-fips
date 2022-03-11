a21c5a99f144a9c23dc89c30cc09cd7a7955d31e
Markku-Juhani O. Saarinen <mjos@mjos.fi>
2022-03-11 09:56:53 +0000

    add LLVM, GPL 3.0

--- riscv_crypto.h
+++ riscv_crypto.h
@@ -1,6 +1,75 @@
 //	riscv_crypto.h
 //	2022-02-12	Markku-Juhani O. Saarinen <mjos@pqshield.com>
-//	Copyright (c) 2022, PQShield Ltd. All rights reserved.
+
+/*
+ *  When including this contribution, use whichever license you seem approriate.
+ *  PQShield has executed FSF assignment/disclaimer process ( #1653644 ).
+ *  for GDB, GCC, GNU, BINUTILS.
+ *
+ *  Best regards: 2022-Mar-11 Markku-Juhani O. Saarinen <mjos@pqshield.com>
+ *  Senior Cryptography Architect, PQShield Ltd., Oxford, UK.
+ *
+ *  General RISC-V license
+ *  ----------------------
+ *
+ *  BSD 2-Clause License
+ *
+ *  Copyright (c) 2021, Markku-Juhani O. Saarinen <mjos@pqshield.com>
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Usage within GPL 3 licensed software:
+ *  -------------------------------------
+ *
+ *  This file is part of GCC <replace if needed>
+ *
+ *  GCC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3, or (at your option)
+ *  any later version.
+ *
+ *  GCC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  Under Section 7 of GPL version 3, you are granted additional
+ *  permissions described in the GCC Runtime Library Exception, version
+ *  3.1, as published by the Free Software Foundation.
+ *
+ *  You should have received a copy of the GNU General Public License and
+ *  a copy of the GCC Runtime Library Exception along with this program;
+ *  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ *  Usage within Apache 2.0 projects
+ *  --------------------------------
+ *
+ *  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+ *  See https://llvm.org/LICENSE.txt for license information.
+ *  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ *
+ */
 
 //	=== Master crypto intrinsics header. Currently Just includes scalar crypto.
 
